b7399b7f46b4b9266c03abbc4f89747f895a7e05
Ana Harris <vana1990@gmail.com>
2018-10-19 10:11:13 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+  hours = time.split(":")[0].to_i
+  minutes = time.split(":")[1].to_i
+  #minutes => 360/60 = 6 degrees per minute
+  #hours => 360/12 = 30 degrees per hour = 0.5 degrees per minute
+  angle = (hours * 30 + minutes * 0.5) - minutes * 6
+  if angle > 180
+    angle = 360 - angle
+  else
+    angle
+  end
+    angle
 end
